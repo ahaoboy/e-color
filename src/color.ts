178a994e7f40ee65838e20f5d271e2b209121ac4
ahaoboy <504595380@qq.com>
2023-12-25 11:32:02 +0800

    0.0.2

--- src/color.ts
+++ src/color.ts
@@ -1,61 +1,136 @@
 import { ColorFormat } from "."
 import {
+  bgrToRgb,
   bgraToRgba,
   invertBGRAColor,
   invertRGBAColor,
+  leftShift,
+  rgbToBgr,
   rgbaToBgra,
+  rightShift,
 } from "./common"
 
 export class Color {
   private color = 0
   private format: ColorFormat = "rgb"
 
-  private constructor(color: number | number = 0, format: ColorFormat = "rgb") {
+  private constructor(color: number | string = 0, format: ColorFormat = "rgb") {
     this.color = typeof color === "number" ? color : parseInt(color, 16)
     this.format = format
   }
   static fromRgba(rgba: string | number) {
-    const c = typeof rgba === "number" ? rgba : parseInt(rgba, 16)
-    return new Color(c, "rgba")
+    return new Color(rgba, "rgba")
   }
 
   static fromBgra(bgra: string | number) {
-    const c = typeof bgra === "number" ? bgra : parseInt(bgra, 16)
-    return new Color(c, "bgra")
+    return new Color(bgra, "bgra")
   }
-  static fromRgb(rgba: string | number) {
-    const c = typeof rgba === "number" ? rgba : parseInt(rgba, 16)
-    return new Color(c, "rgb")
+  static fromRgb(rgb: string | number) {
+    return new Color(rgb, "rgb")
   }
 
-  static fromBgr(bgra: string | number) {
-    const c = typeof bgra === "number" ? bgra : parseInt(bgra, 16)
-    return new Color(c, "bgr")
+  static fromBgr(bgr: string | number) {
+    return new Color(bgr, "bgr")
   }
   toRgba() {
-    if (this.format === "rgba") {
-      return this
+    switch (this.format) {
+      case "rgba": {
+        return this
+      } case "bgra": {
+        this.color = bgraToRgba(this.color)
+        this.format = "rgba"
+        return this
+      } case "rgb": {
+        this.color = leftShift(this.color, 8)
+        this.format = "rgba"
+        return this
+      } case "bgr": {
+        this.color = bgraToRgba(leftShift(this.color, 8))
+        this.format = "rgba"
+        return this
+      }
     }
-    this.color = bgraToRgba(this.color)
-    this.format = "rgba"
-    return this
+
   }
 
   toBgra() {
-    if (this.format === "bgra") {
-      return this
+    switch (this.format) {
+      case "bgra": {
+        return this
+      } case "rgba": {
+        this.color = rgbaToBgra(this.color)
+        this.format = "bgra"
+        return this
+      } case "rgb": {
+        this.color = rgbaToBgra(leftShift(this.color, 8))
+        this.format = "bgra"
+        return this
+      } case "bgr": {
+        this.color = leftShift(this.color, 8)
+        this.format = "bgra"
+        return this
+      }
     }
-    this.color = rgbaToBgra(this.color)
-    this.format = "bgra"
-    return this
   }
 
+  toRgb() {
+    switch (this.format) {
+      case "rgb": {
+        return this
+      } case "rgba": {
+        this.color = rightShift(this.color, 8)
+        this.format = "rgb"
+        return this
+      } case "bgra": {
+        this.color = bgrToRgb(rightShift(this.color, 8))
+        this.format = "rgb"
+        return this
+      } case "bgr": {
+        this.color = bgrToRgb(this.color)
+        this.format = "rgb"
+        return this
+      }
+    }
+  }
+
+  toBgr() {
+    switch (this.format) {
+      case "bgr": {
+        return this
+      } case "bgra": {
+        this.color = this.color >> 8
+        this.format = "bgr"
+        return this
+      } case "rgba": {
+        this.color = rgbToBgr(this.color >> 8)
+        this.format = "bgr"
+        return this
+      } case "rgb": {
+        this.color = rgbToBgr(this.color)
+        this.format = "bgr"
+        return this
+      }
+    }
+  }
+  clone() {
+    return new Color(this.color, this.format)
+  }
   invert() {
-    this.color =
-      this.format === "bgra"
-        ? invertBGRAColor(this.color)
-        : invertRGBAColor(this.color)
-    return this
+    switch (this.format) {
+      case "bgra": {
+        this.color = invertBGRAColor(this.color)
+        return this
+      } case "bgr": {
+        this.color = rightShift(invertBGRAColor(leftShift(this.color, 8)), 8)
+        return this
+      } case "rgb": {
+        this.color = rightShift(invertRGBAColor(leftShift(this.color, 8)), 8)
+        return this
+      } case "rgba": {
+        this.color = invertRGBAColor(this.color)
+        return this
+      }
+    }
   }
   hasAlpha() {
     return this.format === "rgba" || this.format === "bgra"
--- src/common.ts
+++ src/common.ts
@@ -1,43 +1,63 @@
 import { ColorFormat } from "."
-
-// RGBA to BGRA conversion
-export function rgbaToBgra(rgba: number): number {
-  const alpha = (rgba >> 24) & 0xff
-  const red = (rgba >> 16) & 0xff
-  const green = (rgba >> 8) & 0xff
-  const blue = rgba & 0xff
-
-  return (alpha << 24) | (blue << 16) | (green << 8) | red
+export function leftShift(number: number, bits: number): number {
+  return number * Math.pow(2, bits);
+}
+export function rightShift(number: number, bits: number): number {
+  const sign: number = number < 0 ? -1 : 1;
+  const absolute: number = Math.abs(number);
+  return sign * Math.floor(absolute / Math.pow(2, bits));
 }
 
-// BGRA to RGBA conversion
-export function bgraToRgba(bgra: number): number {
-  const alpha = (bgra >> 24) & 0xff
-  const blue = (bgra >> 16) & 0xff
-  const green = (bgra >> 8) & 0xff
-  const red = bgra & 0xff
+export function rgbaToBgra(rgba: number): number {
+  const red = rightShift(rgba, 24) & 0xff
+  const green = rightShift(rgba, 16) & 0xff
+  const blue = rightShift(rgba, 8) & 0xff
+  const alpha = rgba & 0xff
 
-  return (alpha << 24) | (red << 16) | (green << 8) | blue
+  return leftShift(blue, 24) + leftShift(green, 16) + leftShift(red, 8) + alpha
+}
+
+export function rgbToBgr(rgb: number): number {
+  const red = rightShift(rgb, 16) & 0xff
+  const green = rightShift(rgb, 8) & 0xff
+  const blue = rgb & 0xff
+
+  return leftShift(blue, 16) + leftShift(green, 8) + red
+}
+
+export function bgraToRgba(bgra: number): number {
+  const blue = rightShift(bgra, 24) & 0xff
+  const green = rightShift(bgra, 16) & 0xff
+  const red = rightShift(bgra, 8) & 0xff
+  const alpha = bgra & 0xff
+
+  return leftShift(red, 24) + leftShift(green, 16) + leftShift(blue, 8) + alpha
+}
+
+export function bgrToRgb(bgr: number): number {
+  const blue = rightShift(bgr, 16) & 0xff
+  const green = rightShift(bgr, 8) & 0xff
+  const red = bgr & 0xff
+
+  return leftShift(red, 16) + leftShift(green, 8) + blue
 }
 
 export function invertRGBAColor(rgba: number): number {
-  const alpha = (rgba >> 24) & 0xff
-  const red = (rgba >> 16) & 0xff
-  const green = (rgba >> 8) & 0xff
-  const blue = rgba & 0xff
-
+  const red = rightShift(rgba, 24) & 0xff
+  const green = rightShift(rgba, 16) & 0xff
+  const blue = rightShift(rgba, 8) & 0xff
+  const alpha = rgba & 0xff
   return (
-    (alpha << 24) | ((255 - red) << 16) | ((255 - green) << 8) | (255 - blue)
+    leftShift((255 - red), 24) + leftShift((255 - green), 16) + leftShift((255 - blue), 8) + alpha
   )
 }
 export function invertBGRAColor(bgra: number): number {
-  const alpha = (bgra >> 24) & 0xff
-  const blue = (bgra >> 16) & 0xff
-  const green = (bgra >> 8) & 0xff
-  const red = bgra & 0xff
-
+  const blue = rightShift(bgra, 24) & 0xff
+  const green = rightShift(bgra, 16) & 0xff
+  const red = rightShift(bgra, 8) & 0xff
+  const alpha = bgra & 0xff
   return (
-    (alpha << 24) | ((255 - blue) << 16) | ((255 - green) << 8) | (255 - red)
+    leftShift((255 - blue), 24) + leftShift((255 - green), 16) + leftShift((255 - red), 8) + alpha
   )
 }
 
